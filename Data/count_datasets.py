06c98d3ee52b45ce4845eb5b357a1321010d27b1
Stefanie Maierhofer <stefanie_maierhofer@outlook.de>
2023-12-02 15:31:36 +0100

    Update Code for Data preparation

--- Data/count_datasets.py
+++ Data/count_datasets.py
@@ -1,9 +1,9 @@
 import csv
 
-with open('Delimiter_NewLine.txt', 'r', encoding='utf-8') as file:
+with open('../Delimiters/Delimiter_NewLine.txt', 'r', encoding='utf-8') as file:
     delimiter_newline = file.read()
 
-with open('Delimiter_Tab.txt', 'r', encoding='utf-8') as file:
+with open('../Delimiters/Delimiter_Tab.txt', 'r', encoding='utf-8') as file:
     delimiter_tab = file.read()
 
 files = [['filename.tsv'], ['filename2.tsv']]
@@ -13,7 +13,7 @@ for file in files:
     tab_count = 0
     with open(filename, 'r', encoding='utf-16') as infile:
         for line in infile:
-            tab_count += line.count(my_delimiter_tab)
+            tab_count += line.count(delimiter_tab)
         print(filename + ", Tab-Stopps " + str(tab_count + 1))
     with open(filename, 'r', encoding='utf-16') as infile:
         line_count = len(infile.readlines())
